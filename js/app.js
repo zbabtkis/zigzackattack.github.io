f3aecc4aa184711f98f490c89b01df997c647b87
zigzackattack <zackbabtkis@gmail.com>
2014-12-23 10:44:01 -0800

    fix bug where profile items would overlapped

--- js/app.js
+++ js/app.js
@@ -1,7 +1,42 @@
 (function() {
   'use strict';
 
-  $('#portfolio-grid').masonry({
-    itemSelector: '.column'
-  });
+  var $images = $('img');
+
+  function ImagePreloader() {
+    this.totalImagesLoaded = 0;
+    this.deferred = $.Deferred();
+  }
+
+  ImagePreloader.prototype.preload = function($images) {
+    var _this = this;
+
+    this.totalImagesOnPage = $images.length;
+    
+    $images.each(function() {
+      if(this.complete) {
+        _this.onImageLoaded();
+      } else {
+        this.onload = $.proxy(_this, 'onImageLoaded');
+      }
+    });
+
+    return this.deferred.promise();
+  };
+
+  ImagePreloader.prototype.onImageLoaded = function() {
+    this.totalImagesLoaded++;
+
+    if(this.totalImagesLoaded === this.totalImagesOnPage) {
+      this.deferred.resolve();
+    }
+  };
+
+  new ImagePreloader()
+    .preload($images)
+    .then(function() {
+      $('#portfolio-grid').masonry({
+        itemSelector: '.column'
+      });
+    });
 })();
